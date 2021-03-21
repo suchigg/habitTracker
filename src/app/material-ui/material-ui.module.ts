59a19875559ac6a56b9bd85ced3e93fc25efa20c
Luca Farsetti <luca.farsetti@lascaux.it>
2021-03-21 21:50:55 +0100

    Create toolbar
    Created and style toolbar for the application

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
-export const components = [];
+export const components = [
+  MatToolbarModule,
+  MatIconModule
+];
 
 @NgModule({
   imports: [components],
